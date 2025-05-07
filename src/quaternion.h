195112cd8202521dbe8419e8e1b0cbdc5cc4520a
Autumn Pan <pan662607@gmail.com>
2025-05-07 16:18:09 -0400

    define quaternion class

---
+++ src/quaternion.h
@@ -0,0 +1,14 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+class Quaternion
+{
+    public:
+        Quaternion(float q1, float q2, float q3, float q4);
+    protected:
+        float q1;
+        float q2;
+        float q3;
+        float q4;
+};
+
+#endif QUATERNION_H
